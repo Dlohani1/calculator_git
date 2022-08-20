2dcabe3a7bc94da2f95fa5d7baa1e4defef85da8
Ajay <ajchinnivar@gmail.com>
2022-08-20 07:51:47 +0530

    addition file created

---
+++ add.js
@@ -0,0 +1,8 @@
+function add() {
+
+var num1 = document.getElementById("firstno").value
+var num2 = document.getElementById("firstno").value
+var result = num1+num2
+document.getElementById("result").value = result
+
+}
